4e605fdb2cc06facfc9b00c8b2a49518f82d63a4
Milesh Soni <mileshsoni6915@gmail.com>
2023-03-12 15:51:26 +0530

    add user schema

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 const express = require("express");
 const app = express();
+
 // library required to use layouts
 const expressLayout = require("express-ejs-layouts");
+
 // accessing db
 const db = require("./config/mongoose");
-// by default websites run on port 80
+
+// server will run on this port
 const port = 8000;
+
 // To use layouts
 app.use(expressLayout);
 
 // to extract styles and scripts from individual pages
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
+
 // getting the express router function to be called using the middleware
 app.use("/", require("./routes"));
 
+// mentioning folder to look up for static files
 app.use(express.static("./assets"));
-//to set up view engine
+
+//setting up view engine
 app.set("view engine", "ejs");
 app.set("views", "./views");
+
 app.listen(port, function(error){
     if(error){
         console.log(`error in running the server ${error}`);
